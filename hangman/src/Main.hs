638b9b1768eb6952ea49855da32886d7d554b46c
Jesse Williamson <Jesse@thunklife.local>
2020-02-04 19:21:01 -0800

    get a random word

--- hangman/src/Main.hs
+++ hangman/src/Main.hs
@@ -12,11 +12,26 @@ type WordList = [String]
 allWords :: IO WordList
 allWords = lines <$> readFile "data/dict.txt"
 
-minWordLength :: Integer
+minWordLength :: Int
 minWordLength = 5
 
-maxWordLength :: Integer
+maxWordLength :: Int
 maxWordLength = 9
+
+gameWords :: IO WordList
+gameWords = filter gameLength <$> allWords
+  where
+    gameLength :: String -> Bool
+    gameLength s = 
+      let l = length s
+      in l >= minWordLength && l < maxWordLength
+
+randomWord' :: WordList -> IO String
+randomWord' wl = (wl !!) <$> randomRIO (0, l)
+    where l = length wl - 1
+
+randomWord :: IO String
+randomWord = gameWords >>= randomWord'
 
 main :: IO ()
 main = forever $ getLine >>= putStrLn
