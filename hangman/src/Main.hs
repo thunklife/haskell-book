2cbcd3f5335f789f9927aaf0de8103bf0ee5bebe
Jesse Williamson <Jesse@thunklife.local>
2020-02-12 18:28:58 -0800

    almost done

--- hangman/src/Main.hs
+++ hangman/src/Main.hs
@@ -1,11 +1,12 @@
 module Main where
 
-import Control.Monad (forever)
+import Control.Monad (forever, when)
 import Data.Char (toLower)
 import Data.Maybe (isJust)
 import Data.List (intersperse)
 import System.Exit (exitSuccess)
 import System.Random (randomRIO)
+import System.IO
 
 type WordList = [String]
 
@@ -63,5 +64,49 @@ randomWord' wl = (wl !!) <$> randomRIO (0, l)
 randomWord :: IO String
 randomWord = gameWords >>= randomWord'
 
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do
+  putStrLn $ "Your guess was: " ++ [guess]
+  case (charInWord puzzle guess, alreadyGuessed puzzle guess) of
+    (_, True) -> do
+      putStrLn "You already guessed that character, pick something else!"
+      return puzzle
+    (True, _) -> do
+      putStrLn "This character was in the word, filling in the word accordingly"
+      return (fillInCharacter puzzle guess)
+    (False, _) -> do
+      putStrLn "This character wasn't in the word, try again."
+      return (fillInCharacter puzzle guess)
+
+gameOver :: Puzzle -> IO ()
+gameOver (Puzzle wordToGuess _ guessed) =
+   when (length guessed > length wordToGuess) $
+    do putStrLn "You lose!"
+       putStrLn $ "The word was: " ++ wordToGuess
+       exitSuccess
+
+gameWin :: Puzzle -> IO ()
+gameWin (Puzzle wordToGuess filledInSoFar _) =
+  when (all isJust filledInSoFar) $
+    do putStrLn "You win!"
+       putStrLn $ "The word was: " ++ wordToGuess
+       exitSuccess
+
+runGame :: Puzzle -> IO ()
+runGame puzzle = forever $ do
+  gameOver puzzle
+  gameWin puzzle
+  putStrLn $ "Current puzzle is: " ++ show puzzle
+  putStr "Guess a letter: "
+  guess <- getLine
+  case guess of
+    [c] -> handleGuess puzzle c >>= runGame
+    _ -> putStrLn "Your guess must be a single character"
+
 main :: IO ()
-main = forever $ getLine >>= putStrLn
+main = do
+  hSetBuffering stdout NoBuffering
+  word <- randomWord
+  let puzzle = freshPuzzle $ fmap toLower word
+  runGame puzzle
+
